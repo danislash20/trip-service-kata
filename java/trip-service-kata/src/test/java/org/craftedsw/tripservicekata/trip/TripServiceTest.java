77372462818354a7d88346a25b867e386ebd6a7d
Daniel Royo <danislash@gmail.com>
2019-12-12 21:01:46 +0100

    first commit with junit depency

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,14 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.junit.Test;
+
 public class TripServiceTest {
+    @Test
+    public void xxx_testName_xxx(){
+    }
+
+    @Test
+    public void aaaa(){
+    }
 	
 }
